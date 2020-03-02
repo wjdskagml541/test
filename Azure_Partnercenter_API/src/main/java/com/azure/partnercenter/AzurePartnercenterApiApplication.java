7d83b720b6ba148500b3997b6a554a5fba18ce10
wjdskagml541 <tnfltnfl541@gmail.com>
2020-03-02 18:05:06 +0900

    AzurePartnercenterApiApplication

--- Azure_Partnercenter_API/src/main/java/com/azure/partnercenter/AzurePartnercenterApiApplication.java
+++ Azure_Partnercenter_API/src/main/java/com/azure/partnercenter/AzurePartnercenterApiApplication.java
@@ -2,11 +2,18 @@ package com.azure.partnercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("azure.properties")
-public class AzurePartnercenterApiApplication {
+public class AzurePartnercenterApiApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AzurePartnercenterApiApplication.class);
+	}
 	
 	
 	public static void main(String[] args) {
